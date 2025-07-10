cdfc1ee0f397c135d7fd031d069c99b189de3b6e
Sumit Sharma <100469632+sumitkumarsharm@users.noreply.github.com>
2025-07-10 23:46:36 +0530

    Added environment variables for Firebase config and updated firebase initialization to use .env values

--- 10 React Project in 10 Days/ContactApp/src/config/firebase.js
+++ 10 React Project in 10 Days/ContactApp/src/config/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmS4o7M2sn0vCPM0ucyFB5zoS5XrlZDCw",
-  authDomain: "contactapp-35da7.firebaseapp.com",
-  projectId: "contactapp-35da7",
-  storageBucket: "contactapp-35da7.firebasestorage.app",
-  messagingSenderId: "179923431604",
-  appId: "1:179923431604:web:06f553933c46fedef267e1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

46c18ab62a60488bd9cb920d80c01e754eb18621
Sumit Sharma <100469632+sumitkumarsharm@users.noreply.github.com>
2025-07-13 10:24:59 +0530

    This is fd calculator

---
+++ FD Calculator/script.js
@@ -0,0 +1,25 @@
+function calculateMaturityAmount() {
+  // Get input values from the form elements
+
+  const principle = parseFloat(document.getElementById("principle").value);
+  const intersRate = parseFloat(document.getElementById("interestRate").value);
+  const tenure = parseFloat(document.getElementById("tenure").value);
+
+
+  // Perform the Calculation
+
+  const maturityAmount = principle + (principle * intersRate * tenure) / 100;
+  console.log(maturityAmount);
+  
+
+  // Display the Result
+
+  document.getElementById(
+    "result"
+  ).innerText = `Maturity Amount: ${maturityAmount.toFixed(2)}`;
+}
+
+// Attach the event listener to the calculate Button
+document
+  .getElementById("calculateBtn")
+  .addEventListener("click", calculateMaturityAmount);
